97034edb35e92361daaa24512989d00f6c3fd517
Paul Selkirk <paul@psgd.org>
2018-11-26 17:26:55 -0500

    Add loop unrolling to bring the profilable mem* functions closer to newlib, because memset is called a LOT in the course of RSA signing, and we need to understand how much time we're actually spending there.

--- libraries/libprof/memfunc.c
+++ libraries/libprof/memfunc.c
@@ -4,9 +4,15 @@
 /*
  * Profilable substitutes for mem*(), lacking libc_p.a
  *
- * This code was written with reference to newlib, but does not copy every
- * quirk and loop-unrolling optimization from newlib. Its only purpose is
- * to let us figure out who is calling memcpy 2 million times.
+ * This code was written with reference to newlib, and was recently
+ * brought closer into line with newlib, to make profiling more accurate.
+ *
+ * Newlib is maintained by Cygwin, which is Red Hat. There is no copyright
+ * statement in the corresponding newlib source files, nor is there a
+ * COPYING file in newlib/libc/string or newlib/libc. Consider this file
+ * to be covered under one or more of the 50 copyright notices in
+ * newlib/COPYING, most of which are BSD. In any case, this file is only
+ * used for profiling, and is not used in production builds.
  */
 
 #define is_word_aligned(x) (((size_t)(x) & 3) == 0)
@@ -16,12 +22,19 @@ void *memcpy(void *dst, const void *src, size_t n)
     uint8_t *d8 = (uint8_t *)dst;
     uint8_t *s8 = (uint8_t *)src;
 
-    if (n >= 4 && is_word_aligned(src) && is_word_aligned(dst)) {
+    if (n >= sizeof(uint32_t) && is_word_aligned(src) && is_word_aligned(dst)) {
         uint32_t *d32 = (uint32_t *)dst;
         uint32_t *s32 = (uint32_t *)src;
-        while (n >= 4) {
+        while (n >= 4 * sizeof(uint32_t)) {
             *d32++ = *s32++;
-            n -= 4;
+            *d32++ = *s32++;
+            *d32++ = *s32++;
+            *d32++ = *s32++;
+            n -= 4 * sizeof(uint32_t);
+        }
+        while (n >= sizeof(uint32_t)) {
+            *d32++ = *s32++;
+            n -= sizeof(uint32_t);
         }
         d8 = (uint8_t *)d32;
         s8 = (uint8_t *)s32;
@@ -38,12 +51,25 @@ void *memset(void *dst, int c, size_t n)
     uint8_t *d8 = (uint8_t *)dst;
     uint8_t c8 = (uint8_t)c;
 
-    if (n >= 4 && is_word_aligned(dst)) {
-        uint32_t *d32 = (uint32_t *)dst;
+    while (!is_word_aligned(d8)) {
+        if (n--)
+            *d8++ = c8;
+        else
+            return dst;
+    }
+    if (n >= sizeof(uint32_t)) {
+        uint32_t *d32 = (uint32_t *)d8;
         uint32_t c32 = (c8 << 24) | (c8 << 16) | (c8 << 8) | (c8);
-        while (n >= 4) {
+        while (n >= 4 * sizeof(uint32_t)) {
             *d32++ = c32;
-            n -= 4;
+            *d32++ = c32;
+            *d32++ = c32;
+            *d32++ = c32;
+            n -= 4 * sizeof(uint32_t);
+        }
+        while (n >= sizeof(uint32_t)) {
+            *d32++ = c32;
+            n -= sizeof(uint32_t);
         }
         d8 = (uint8_t *)d32;
     }
@@ -59,15 +85,15 @@ int memcmp(const void *dst, const void *src, size_t n)
     uint8_t *d8 = (uint8_t *)dst;
     uint8_t *s8 = (uint8_t *)src;
 
-    if (n >= 4 && is_word_aligned(src) && is_word_aligned(dst)) {
+    if (n >= sizeof(uint32_t) && is_word_aligned(src) && is_word_aligned(dst)) {
         uint32_t *d32 = (uint32_t *)dst;
         uint32_t *s32 = (uint32_t *)src;
-        while (n >= 4) {
+        while (n >= sizeof(uint32_t)) {
             if (*d32 != *s32)
                 break;
             d32++;
             s32++;
-            n -= 4;
+            n -= sizeof(uint32_t);
         }
         d8 = (uint8_t *)d32;
         s8 = (uint8_t *)s32;
